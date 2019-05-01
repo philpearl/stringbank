2aca996fd6594ccb0a073eb2072adcf4879409d9
Phil Pearl <philip.j.r.pearl@gmail.com>
2019-05-01 10:52:04 +0100

    Add an off-heap stringbank

---
+++ offheap/go.mod
@@ -0,0 +1,8 @@
+module github.com/philpearl/stringbank/offheap
+
+go 1.12
+
+require (
+	github.com/philpearl/mmap v0.0.0-20190501094812-b5dc52c98503
+	github.com/stretchr/testify v1.3.0
+)
---
+++ offheap/stringbank_test.go
@@ -0,0 +1,100 @@
+package offheap
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringbank(t *testing.T) {
+	sb := Stringbank{}
+
+	s1 := sb.Save("hello")
+	s2 := sb.Save("goodbye")
+	s3 := sb.Save("cheese")
+
+	assert.Equal(t, "hello", sb.Get(s1))
+	assert.Equal(t, "goodbye", sb.Get(s2))
+	assert.Equal(t, "cheese", sb.Get(s3))
+}
+
+func TestStringbankSize(t *testing.T) {
+	sb := Stringbank{}
+	assert.Zero(t, sb.Size())
+	sb.Save("hello")
+	assert.Equal(t, stringbankSize, sb.Size())
+}
+
+func TestLengths(t *testing.T) {
+	tests := []struct {
+		len int
+	}{
+		{1},
+		{127},
+		{128},
+		{254},
+		{255},
+		{256},
+		{0xFFFFFFFFFF},
+	}
+
+	for _, test := range tests {
+		t.Run(strconv.Itoa(test.len), func(t *testing.T) {
+			buf := make([]byte, 10)
+
+			l := writeLength(test.len, buf)
+			assert.Equal(t, l, spaceForLength(test.len))
+			len, lenlen := readLength(buf)
+			assert.Equal(t, l, lenlen)
+			assert.Equal(t, test.len, len)
+		})
+	}
+}
+
+func TestGC(t *testing.T) {
+	sb := Stringbank{}
+	for i := 0; i < 10000000; i++ {
+		sb.Save(strconv.Itoa(i))
+	}
+	runtime.GC()
+
+	start := time.Now()
+	runtime.GC()
+	assert.True(t, time.Since(start) < 1000*time.Microsecond)
+	runtime.KeepAlive(sb)
+}
+
+func BenchmarkStringbank(b *testing.B) {
+	s := make([]string, b.N)
+	for i := range s {
+		s[i] = strconv.Itoa(i)
+	}
+
+	index := make([]int, b.N)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	sb := Stringbank{}
+	for i, v := range s {
+		index[i] = sb.Save(v)
+	}
+
+	var out string
+	for _, i := range index {
+		out = sb.Get(i)
+	}
+	if out != s[b.N-1] {
+		b.Fatalf("final string should be %s, is %s", s[b.N-1], out)
+	}
+}
+
+func ExampleStringbank() {
+	sb := Stringbank{}
+	i := sb.Save("goodbye")
+	fmt.Println(sb.Get(i))
+	// Output: goodbye
+}
